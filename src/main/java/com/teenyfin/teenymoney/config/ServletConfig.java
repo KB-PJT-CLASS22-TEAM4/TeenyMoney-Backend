68268b394340f5a06bb397fa4775c7e854332b58
김지민 <kimjm9841@naver.com>
2026-08-04 14:46:45 +0900

    refactor: springfox 자동 생성 적용 (#53)

--- src/main/java/com/teenyfin/teenymoney/config/ServletConfig.java
+++ src/main/java/com/teenyfin/teenymoney/config/ServletConfig.java
@@ -38,13 +38,24 @@ public class ServletConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/api-docs/**")
-                .addResourceLocations("classpath:/openapi/");
+        registry
+                .addResourceHandler("/resources/**") // url이 /resources/로 시작하는 모든 경로
+                .addResourceLocations("/resources/"); // webapp/resources/경로로 매핑
 
-        registry.addResourceHandler("/swagger-ui/**")
-                .addResourceLocations(
-                        "classpath:/swagger-ui/",
-                        "classpath:/META-INF/resources/webjars/swagger-ui/5.31.0/"
-          );
+        // Swagger UI 리소스를 위한 핸들러 설정
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        // Swagger WebJar 리소스 설정
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        // Swagger 리소스 설정
+        registry.addResourceHandler("/swagger-resources/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/v2/api-docs")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
     }
 }
