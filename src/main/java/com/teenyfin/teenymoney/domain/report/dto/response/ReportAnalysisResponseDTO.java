c1944137183129cb531d8697d87abe2d1bb7a48e
SunWon <dkzndkqh@naver.com>
2026-08-18 13:28:01 +0900

    feat: 프론트 응답 DTO 추가

---
+++ src/main/java/com/teenyfin/teenymoney/domain/report/dto/response/ReportAnalysisResponseDTO.java
@@ -0,0 +1,16 @@
+package com.teenyfin.teenymoney.domain.report.dto.response;
+
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@ApiModel(description = "이번 달·지난달 금융 습관 AI 분석")
+public class ReportAnalysisResponseDTO {
+
+    @ApiModelProperty(value = "LLLM이 생성한 서술형 분석, 조언 텍스트", example = "이번 달엔 용돈을 받은 날 크게 쓰는 습관이 보였어요.. 등")
+    private final String analysis;
+}
